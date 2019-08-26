cf2ba890a8737c78e683b6536d644537021825bb
rlatkddn212 <rlatkddn212@chromium.org>
2019-08-27 02:38:03 +0900

    Update SimpleScrollView.java
    OverScroll filing 적용

--- app/src/main/java/com/ksw/simple_calender/SimpleScrollView.java
+++ app/src/main/java/com/ksw/simple_calender/SimpleScrollView.java
@@ -3,39 +3,19 @@ package com.ksw.simple_calender;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.OverScroller;
 import android.widget.Toast;
 
-import androidx.core.view.ViewCompat;
-
-import java.util.ArrayList;
-
 public class SimpleScrollView extends View {
-    GestureDetector gestureDetector;
-    VelocityTracker m_velocityTracker;
-    OverScroller overScroller;
-    int AXIS_X_MIN = 0;
-    int AXIS_Y_MIN = 0;
-    int AXIS_X_MAX = 240;
-    int AXIS_Y_MAX = 240;
-    private RectF mScrollerStartViewport =
-            new RectF(AXIS_X_MIN, AXIS_Y_MIN, AXIS_X_MAX, AXIS_Y_MAX);
-    private RectF mCurrentViewport =
-            new RectF(AXIS_X_MIN, AXIS_Y_MIN, AXIS_X_MAX, AXIS_Y_MAX);
-
-    int startX;
-    int startY;
-    int index = 0;
-    ArrayList<Integer> contents;
+    GestureDetector mGestureDetector;
+    OverScroller mOverScroller;
+    int mStartScroll;
 
     public SimpleScrollView(Context context) {
         super(context);
@@ -53,22 +33,12 @@ public class SimpleScrollView extends View {
     }
 
     private void init(final Context context) {
-        contents = new ArrayList<Integer>(5);
-        int number = 1;
-        for (Integer i : contents){
-            i = number;
-            number++;
-        }
-
-        overScroller = new OverScroller(context, new LinearInterpolator());
-        gestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+        mOverScroller = new OverScroller(context, new LinearInterpolator());
+        mGestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
             @Override
             public boolean onDown(MotionEvent motionEvent) {
-                overScroller.forceFinished(true);
-                mScrollerStartViewport.set(mCurrentViewport);
-                startX = (int)motionEvent.getX();
-                startY = (int)motionEvent.getY();
-                //Toast.makeText(context, "on Dowm", Toast.LENGTH_SHORT).show();
+                mOverScroller.forceFinished(true);
+                mStartScroll = (int)getScrollY();
                 return false;
             }
 
@@ -87,62 +57,60 @@ public class SimpleScrollView extends View {
             public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
                 Log.d("onScroll","onScroll : " + motionEvent.getY() + "/" + motionEvent1.getY() + "/" + v +"/" + v1);
                //scroll(motionEvent, motionEvent1);
-                offsetTopAndBottom((int)motionEvent1.getY() - (int)motionEvent.getY());
-                return false;
+                setScrollY((int)-(motionEvent1.getY() - motionEvent.getY()) + mStartScroll);
+                invalidate();
+                return true;
             }
 
             @Override
             public void onLongPress(MotionEvent motionEvent) {
-                Toast.makeText(context, "onLongPress", Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
             public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                int currentY = getScrollY();
+                int targetY = 0;
+                if (v1 > 0) {
+                    if (currentY <= 0) {
+                        return false;
+                    }
 
-                Log.d("onFling","onFling : " + startX + "/" + startY + "/" + v +"/" + v1);
-                overScroller.startScroll(getScrollX(), getScrollY(), -getScrollX(), -getScrollY());
-                return false;
+                    targetY = currentY / 1000 * 1000;
+                }
+                else {
+                    if (currentY >= 1000 * (3 - 1)) {
+                        return false;
+                    }
+
+                    targetY = 1000 * (currentY / 1000 + 1);
+                }
+
+                if (currentY == targetY) {
+                    targetY -= 1000;
+                }
+
+                Log.d("fling","fling : " + currentY + "/" + v1 + "/" + targetY );
+                mOverScroller.fling (0, currentY, 0, -(int) v1,
+                        0, 0, Math.min (currentY, targetY), Math.max (currentY, targetY));
+                invalidate();
+                return true;
             }
         });
     }
 
     @Override
     public void computeScroll() {
-        Log.d("computeScroll","computeScroll : " + overScroller.getCurrX() + "/" + overScroller.getCurrY());
         super.computeScroll();
-        //setY(overScroller.getCurrY());
-        invalidate();
-        //int currY = overScroller.getCurrY();
+        if (mOverScroller.computeScrollOffset ()) {
+            Log.d("computeScroll","computeScroll : " + mOverScroller.getCurrX() + "/" + mOverScroller.getCurrY());
+            scrollTo (0, mOverScroller.getCurrY());
+            invalidate();
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        /*
-        if (m_velocityTracker == null) {
-            m_velocityTracker = VelocityTracker.obtain();
-        }
-
-        m_velocityTracker.addMovement(event);
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                break;
-            case MotionEvent.ACTION_MOVE:
-                m_velocityTracker.computeCurrentVelocity(1);
-                float velocity = m_velocityTracker.getXVelocity();
-                break;
-            case MotionEvent.ACTION_UP:
-                if (m_velocityTracker != null) {
-                    m_velocityTracker.recycle();
-                    m_velocityTracker = null;
-                }
-
-                break;
-        }
-        return true;
-        */
-        gestureDetector.onTouchEvent(event);
+        mGestureDetector.onTouchEvent(event);
         return true;
     }
 
