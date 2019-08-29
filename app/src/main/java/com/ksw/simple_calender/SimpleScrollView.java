d8eeecd3c4faca4fa79ad774717d98dd04b44b0b
rlatkddn212 <rlatkddn212@chromium.org>
2019-08-29 20:58:01 +0900

    Update SimpleScrollView.java

--- app/src/main/java/com/ksw/simple_calender/SimpleScrollView.java
+++ app/src/main/java/com/ksw/simple_calender/SimpleScrollView.java
@@ -10,8 +10,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.OverScroller;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class SimpleScrollView extends View {
